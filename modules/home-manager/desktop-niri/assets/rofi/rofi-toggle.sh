2fc87f1cbd45baa2e61749d28089a0a465fef503
Catalin Balan <laserbeam3@users.noreply.github.com>
2025-08-10 13:58:38 +0300

    Rofi should have the DISPLAY var set in order to start X apps

--- modules/home-manager/desktop-niri/assets/rofi/rofi-toggle.sh
+++ modules/home-manager/desktop-niri/assets/rofi/rofi-toggle.sh
@@ -3,5 +3,7 @@
 if pgrep -x rofi; then
     pkill rofi
 else
-    rofi -show drun -config ~/.config/rofi/rofi-default.rasi
+    # Rofi needs to launch apps made for X (instead of wayland) sometimes, and
+    # those need DISPLAY=:0 set.
+    DISPLAY=:0 rofi -show drun -config ~/.config/rofi/rofi-default.rasi
 fi
