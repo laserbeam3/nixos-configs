122ea496149029a227ad63b40fb61b6d63948564
Catalin Balan <laserbeam3@users.noreply.github.com>
2025-08-19 13:29:52 +0300

    Remove some comments from the default niri config

--- modules/home-manager/desktop-niri/default.hm.nix
+++ modules/home-manager/desktop-niri/default.hm.nix
@@ -393,14 +393,6 @@
         Mod+Equal       { set-column-width "+10%"; }
         Mod+Shift+Minus { set-window-height "-10%"; }
         Mod+Shift+Equal { set-window-height "+10%"; }
-
-        // Actions to switch layouts.
-        // Note: if you uncomment these, make sure you do NOT have
-        // a matching layout switch hotkey configured in xkb options above.
-        // Having both at once on the same hotkey will break the switching,
-        // since it will switch twice upon pressing the hotkey (once by xkb, once by niri).
-        // Mod+Space       { switch-layout "next"; }
-        // Mod+Shift+Space { switch-layout "prev"; }
       }
     '';
 
